43caf87e68b52415cf719ff1008b083cdcedc090
bjliubo <bjliubo@corp.netease.com>
2019-12-30 16:31:29 +0800

    代码构建

---
+++ src/main/java/com/lbo/code/timeqinsociety/init/InitRunner.java
@@ -0,0 +1,12 @@
+package com.lbo.code.timeqinsociety.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... strings) throws Exception {
+    }
+}
